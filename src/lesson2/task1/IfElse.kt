1275ae714ac12311a525acc698d20a7212fdac95
Петр <ppo@ciklum.com>
2018-04-20 16:03:11 +0300

    Couple more tasks finished

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -1,4 +1,5 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson2.task1
 
 import lesson1.task1.discriminant
@@ -33,7 +34,19 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String {
+    val ageLastNumber = age % 10
+    val exclusion = age % 100 in 11..14
+    var old = ""
+    if (exclusion) old = "$age лет" else
+        when (ageLastNumber) {
+            0,
+            in 5..9 -> old = "$age лет"
+            in 2..4 -> old = "$age года"
+            1 -> old = "$age год"
+        }
+    return old
+}
 
 /**
  * Простая
